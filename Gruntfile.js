02ddc2aeca4800212aed9782112eb75d02dba9e6
Albino Tonnina <overnoise@gmail.com>
2014-04-13 23:45:31 +0200

    no message

--- Gruntfile.js
+++ Gruntfile.js
@@ -167,7 +167,7 @@ module.exports = function (grunt) {
                         '<%= config.dist %>/scripts/{,*/}*.js',
                         '<%= config.dist %>/styles/{,*/}*.css',
                         '<%= config.dist %>/images/{,*/}*.*',
-                        '<%= config.dist %>/styles/fonts/{,*/}*.*',
+                
                         '<%= config.dist %>/*.{ico,png}'
                     ]
                 }
