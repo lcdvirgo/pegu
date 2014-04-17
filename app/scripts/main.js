8f24418849225b3467aacbe2b7fe03c023594b6e
Albino Tonnina <overnoise@gmail.com>
2014-04-18 01:54:17 +0200

    no message

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,35 +1,39 @@
-
-
 jQuery(document).ready(function($) {
-    game = new Game(LocalStorage,Social,Sound);
+    var game = new Game(LocalStorage, Social, Sound);
     if (game.getGameStatus() == 1) {
-        $('header').hide();
-        $('.game_asset').show('500',function(){
-            
+        $('header').hide('500', function() {
+            game.start();
         });
-        game.start();
+        $('.game_asset').show('500');
     } else {
         $('header').show('500');
         $('.game_asset').hide('500');
     }
     $('.play a').on('click', function(event) {
         event.preventDefault();
-        $('header').hide('500');
-        $('.game_asset').show('500',function(){
-
-
-
+        $('.how_to_play').hide('200');
+        $('header').hide('500', function() {
+            if (game.getGameStatus() == 1) {} else {
+                game.start();
+            }
         });
-
-
-        if (game.getGameStatus() == 1) {} else {
-            game.start();
-        }
-
-
+        $('.game_asset').show('500');
     });
+    $('.how_to_play a').on('click', function(event) {
+        event.preventDefault();
+        $('.how_to_play').hide('200');
+        $('header').hide('500', function() {
+            game.tutorial();
+        });
+        $('.game_asset').show('500');
+    });
+
+
     $('#infobtn').on('click', function(event) {
         event.preventDefault();
+
+        $('.how_to_play').show('200');
+
         $('header').show('500');
         $('.game_asset').hide('500');
     });
@@ -45,19 +49,14 @@ jQuery(document).ready(function($) {
         event.preventDefault();
         game.previousLevel();
     });
-
     $('#sharebtn').on('click', function(event) {
         event.preventDefault();
-
-if(game.isopenSocial()){
-game.closeSocial();
-}else{
-    game.openSocial();
-}
-
-        
+        if (game.isopenSocial()) {
+            game.closeSocial();
+        } else {
+            game.openSocial();
+        }
     });
-
     $(window).unload(function() {
         game.saveState();
     });
